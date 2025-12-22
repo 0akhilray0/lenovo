2223012bff77105e2728cc3d8d28f3cda32e612c
0akhilray0 <akhilkumarsray2000@gmail.com>
2025-12-22 22:30:52 +0530

    updating the config

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias ll 'ls -la'
+alias windows 'sudo grub-reboot "Windows11" and sudo reboot'
+set -Ux EDITOR nvim
